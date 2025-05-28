56b65430d651f5941df4d3c5bccdb35840251a8e
CUBOVR <cristobalurquidesb@gmail.com>
2025-05-27 23:38:40 -0600

    modificación componente selectedHeight

--- lib/components/heightSelector.dart
+++ lib/components/heightSelector.dart
@@ -3,14 +3,20 @@ import 'package:imc_calculator/core/app_colors.dart';
 import 'package:imc_calculator/core/text_styles.dart';
 
 class HeightSelector extends StatefulWidget {
-  const HeightSelector({super.key});
+  final double height;
+  final Function(double) onHeightChanged;
+
+  const HeightSelector({
+    super.key,
+    required this.height,
+    required this.onHeightChanged,
+  });
 
   @override
   State<HeightSelector> createState() => _HeightSelectorState();
 }
 
 class _HeightSelectorState extends State<HeightSelector> {
-  double height = 140; // Altura inicial
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -27,20 +33,18 @@ class _HeightSelectorState extends State<HeightSelector> {
               child: Text("ALTURA", style: TextStyles.bodyText),
             ),
             Text(
-              "${height.toStringAsFixed(0)} cm",
+              "${widget.height.toStringAsFixed(0)} cm",
               style: TextStyles.principalText,
             ),
             Slider(
-              value: height,
+              value: widget.height,
               onChanged: (newHeight) {
-                setState(() {
-                  height = newHeight;
-                });
+                widget.onHeightChanged(newHeight);
               },
               min: 130,
               max: 220,
               divisions: 90,
-              label: "${height.toStringAsFixed(0)} cm",
+              label: "${widget.height.toStringAsFixed(0)} cm",
               activeColor: AppColors.primary,
             ),
           ],
--- lib/screens/imc_homeScreen.dart
+++ lib/screens/imc_homeScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:imc_calculator/components/genderSelector.dart';
 import 'package:imc_calculator/components/heightSelector.dart';
 import 'package:imc_calculator/components/numberSelector.dart';
+import 'package:imc_calculator/core/app_colors.dart';
+import 'package:imc_calculator/core/text_styles.dart';
 
 class ImcHomescreen extends StatefulWidget {
   const ImcHomescreen({super.key});
@@ -13,13 +15,21 @@ class ImcHomescreen extends StatefulWidget {
 class _ImcHomescreenState extends State<ImcHomescreen> {
   int selectedAge = 24;
   int selectedWeight = 63;
+  double selectedHeight = 140;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Genderselector(),
-        HeightSelector(),
+        HeightSelector(
+          height: selectedHeight,
+          onHeightChanged: (newHeight) {
+            setState(() {
+              selectedHeight = newHeight;
+            });
+          },
+        ),
         Padding(
           padding: const EdgeInsets.all(16.0),
           child: Row(
@@ -58,6 +68,26 @@ class _ImcHomescreenState extends State<ImcHomescreen> {
                 ),
               ),
             ],
+          ),
+        ),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                backgroundColor: WidgetStateProperty.all(AppColors.primary),
+              ),
+              child: Text("Calcular", style: TextStyles.bodyText),
+            ),
           ),
         ),
       ],
